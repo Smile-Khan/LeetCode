75d2fddcca3b0c2b8ae70e31339772af710eee07
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2023-10-06 19:24:14 +0530

    Create 343. Integer Break.java

---
+++ MEDIUM/343. Integer Break.java
@@ -0,0 +1,34 @@
+QUESTION :- class 343. Integer Break 
+
+https://leetcode.com/problems/integer-break/description/?envType=daily-question&envId=2023-10-06
+
+
+
+SOLUTION :-
+
+
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        
+        int[] dp = new int[n + 1];
+
+        // Set base cases
+        for (int i = 1; i <= 3; i++) {
+            dp[i] = i;
+        }
+        
+        for (int num = 4; num <= n; num++) {
+            int ans = num;
+            for (int i = 2; i < num; i++) {
+                ans = Math.max(ans, i * dp[num - i]);
+            }
+            
+            dp[num] = ans;
+        }
+        
+        return dp[n];
+    }
+}
