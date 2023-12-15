36ed15b10c25fb606e6c761c69baf5c6cd8e7442
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2023-12-15 23:21:35 +0530

    Create 1436. Destination City.java

---
+++ EASY/1436. Destination City.java
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/destination-city/description/?envType=daily-question&envId=2023-12-15
+
+
+
+SOLUTION :-
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        for (int i = 0; i < paths.size(); i++) {
+            String candidate = paths.get(i).get(1);
+            boolean good = true;
+            
+            for (int j = 0; j < paths.size(); j++) {
+                if (paths.get(j).get(0).equals(candidate)) {
+                    good = false;
+                    break;
+                }
+            }
+
+            if (good) {
+                return candidate;
+            }
+        }
+        
+        return "";
+    }
+}
