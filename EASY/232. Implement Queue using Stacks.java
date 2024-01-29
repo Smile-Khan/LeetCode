a674071fb84174c58f18fd68ad872da174c9412f
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2024-01-29 20:38:01 +0530

    Create 232. Implement Queue using Stacks.java

---
+++ EASY/232. Implement Queue using Stacks.java
@@ -0,0 +1,65 @@
+public class 232. Implement Queue using Stacks {
+ 
+    
+    https://leetcode.com/problems/implement-queue-using-stacks/description/?envType=daily-question&envId=2024-01-29
+
+import java.util.Stack;
+
+}
+
+
+SOLUTION :-
+
+class MyQueue {
+
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+    private int peekEl;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+        peekEl = -1;
+    }
+
+    public void push(int x) {
+        if (input.empty()) {
+            peekEl = x;
+        }
+        input.push(x);
+    }
+
+    // Amortized O(1)
+    public int pop() {
+        if (output.empty()) {
+            // Transfer elements from input to output (O(n))
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+
+        int val = output.pop();
+        return val;
+    }
+
+    public int peek() {
+        if (output.empty()) {
+            return peekEl;
+        }
+
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
