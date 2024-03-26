979eb7aba1ded3d397754ebe3cc9a8dfc2b2a097
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2024-03-26 23:30:15 +0530

    Create 41. First Missing Positive.java

---
+++ Array/41. First Missing Positive.java
@@ -0,0 +1,38 @@
+package Array;
+
+public class 41. First Missing Positive {
+    
+
+    https://leetcode.com/problems/first-missing-positive/description/?envType=daily-question&envId=2024-03-26
+
+
+}
+
+
+SOLUTION :-
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1]; // Array for lookup
+
+        // Mark the elements from nums in the lookup array
+        for (int num : nums) {
+            if (num > 0 && num <= n) {
+                seen[num ] = true;
+            }
+        }
+
+        // Iterate through integers 1 to n
+        // return smallest missing positive integer
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                return i;
+            }
+        }
+
+        // If seen contains all elements 1 to n
+        // the smallest missing positive number is n + 1
+        return n + 1;
+    }
+}
