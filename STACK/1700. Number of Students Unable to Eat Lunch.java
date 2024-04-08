6ac8f38c0c58b042e1c497b91b6ea768bfdf619c
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2024-04-08 21:33:34 +0530

    Create 1700. Number of Students Unable to Eat Lunch.java

---
+++ STACK/1700. Number of Students Unable to Eat Lunch.java
@@ -0,0 +1,45 @@
+package STACK;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class 1700. Number of Students Unable to Eat Lunch {
+    
+    https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/description/?envType=daily-question&envId=2024-04-08
+
+}
+
+SOLUTION :-
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int len = students.length; // Sandwiches will be the same length
+        Queue<Integer> studentQueue = new LinkedList<>();
+        Stack<Integer> sandwichStack = new Stack<>();
+        
+        // Add students and sandwiches to the queue and stack
+        for (int i = 0; i < len; i++) {
+            sandwichStack.push(sandwiches[len - i - 1]);
+            studentQueue.offer(students[i]);
+        }
+
+        // Simulate the lunch process by serving sandwiches 
+        // or sending students to the back of the queue.
+        int lastServed = 0;
+        while (studentQueue.size() > 0 && lastServed < studentQueue.size()) {
+            if (sandwichStack.peek() == studentQueue.peek()) {
+                sandwichStack.pop(); // Serve sandwich
+                studentQueue.poll(); // Student leaves queue
+                lastServed = 0;
+            } else {
+                // Student moves to back of queue
+                studentQueue.offer(studentQueue.poll()); 
+                lastServed++;
+            }
+        }
+
+        // Remaining students in queue are unserved students
+        return studentQueue.size();    
+    }
+}
