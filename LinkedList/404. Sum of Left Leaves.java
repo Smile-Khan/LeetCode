fa8ba452f7c34fa729b3f528f20bf226f10df0da
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2024-04-14 19:25:36 +0530

    Create 404. Sum of Left Leaves.java

---
+++ LinkedList/404. Sum of Left Leaves.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+import javax.swing.tree.TreeNode;
+
+public class 404. Sum of Left Leaves {
+    
+    https://leetcode.com/problems/sum-of-left-leaves/description/?envType=daily-question&envId=2024-04-14
+
+}
+
+
+SOLUTION :-
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sum(TreeNode curr, TreeNode parent) {
+       if (curr == null)
+           return 0;
+
+       int left = sum(curr.left, curr);
+       int right = sum(curr.right, curr);
+
+       int sum = 0;
+       if (curr.left == null && curr.right == null) {
+           if (parent != null && parent.left == curr)
+               sum += curr.val;
+       }
+
+       return left + right + sum;
+   }
+
+   public int sumOfLeftLeaves(TreeNode root) {
+       if (root == null)
+           return 0;
+       TreeNode parent = null;
+       return sum(root, parent);
+   }
+}
